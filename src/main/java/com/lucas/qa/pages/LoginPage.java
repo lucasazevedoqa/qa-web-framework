5f51f8f9b76f6bf09af566a7ff45c3304de705f7
Lucas Azevedo <lucasworkingqa@gmail.com>
2026-06-15 12:19:32 -0300

    feat: estrutura inicial do framework com Selenium e JUnit 5

---
+++ src/main/java/com/lucas/qa/pages/LoginPage.java
@@ -0,0 +1,28 @@
+package com.lucas.qa.pages;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    // 1. Variável para guardar o driver (navegador)
+    private WebDriver driver;
+
+    // 2. Definindo os elementos da página (Localizadores)
+    // Usamos 'By' para encontrar elementos na tela
+    private By userField = By.id("user-name");
+    private By passwordField = By.id("password");
+    private By loginButton = By.id("login-button");
+
+    // 3. Construtor: Recebe o driver que está vindo do seu teste
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    // 4. Ação: O que  o usuário faz nesta página
+    public void realizaLogin(String usuario, String senha){
+        driver.findElement(userField).sendKeys(usuario);
+        driver.findElement(passwordField).sendKeys(senha);
+        driver.findElement(loginButton).click();
+    }
+}
---
+++ src/test/java/com/lucas/qa/tests/LoginComFailureTest.java
@@ -0,0 +1,30 @@
+package com.lucas.qa.tests;
+
+import org.junit.jupiter.api.Test;
+import com.lucas.qa.pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginComFailureTest {
+
+    @Test
+    void validarLoginComSucesso() throws InterruptedException {
+        // 1. Inicializar o navegador
+        WebDriver driver = new ChromeDriver();
+
+        // 2. Abre o Site
+        driver.get("https://www.saucedemo.com/");
+
+        // 3. Criar o objeto da página (aqui conectamos o navegador a página)
+        LoginPage loginPage = new LoginPage(driver);
+
+        // 4. Executar a ação
+        loginPage.realizaLogin("standard_user", "secret_sau131ce");
+
+        // 5. Pausa
+        Thread.sleep(20000);
+
+        // 6. Encerrar navegador
+        driver.quit();
+    }
+}
