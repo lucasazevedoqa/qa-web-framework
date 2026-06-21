1609200894d0c18aada5068eb94d627e31b5dcaa
Lucas Azevedo <lucasworkingqa@gmail.com>
2026-06-20 22:52:56 -0300

    refactor: adiciona BeforeEach e AfterEach aos testes

--- src/test/java/com/lucas/qa/tests/LoginComFailureTest.java
+++ src/test/java/com/lucas/qa/tests/LoginComFailureTest.java
@@ -5,37 +5,53 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import com.lucas.qa.pages.LoginPage;
 import com.lucas.qa.drivers.DriverFactory;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 
 public class LoginComFailureTest {
+
+    // Driver compartilhado por todos os métodos da classe
+    private WebDriver driver;
+
+    @BeforeEach
+    void setup(){
+
+        // Criar o Navegador
+        driver = DriverFactory.criarDriver();
+
+        // Acessar o Sistema
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterEach
+    void tearDown(){
+
+        // Fechar o navegador após cada teste
+        driver.quit();
+    }
 
     @Test
     void validarLoginComSucesso() throws InterruptedException {
 
-        // 1. Obtém uma instância do navegador através da fábrica
-        WebDriver driver = DriverFactory.criarDriver();
-
-        // 2. Abre o Site
-        driver.get("https://www.saucedemo.com/");
-
-        // 3. Criar o objeto da página (aqui conectamos o navegador a página)
+        // Criar o objeto da página (aqui conectamos o navegador a página)
         LoginPage loginPage = new LoginPage(driver);
 
-        // 4. Executar a ação
-        loginPage.realizaLogin("standard_user", "secret_ERRO");
+        // Executar a ação
+        loginPage.realizaLogin(
+                "standard_user",
+                "secret_ERRO"
+        );
 
-        // 5. Captura a mensagem de erro exibida na tela
+        // Captura a mensagem de erro exibida na tela
         String mensagemErro = loginPage.obterMensagemErro();
 
-        // 6. Verifica se a mensagem contém o texto esperado
+        // Verifica se a mensagem contém o texto esperado
         Assertions.assertTrue(
                 mensagemErro.contains("Username and password do not match")
         );
 
-        // 7. Pausa
+        // Pausa
         Thread.sleep(2000);
 
-        // 8. Encerrar navegador
-        driver.quit();
     }
 }
--- src/test/java/com/lucas/qa/tests/LoginComSucessoTest.java
+++ src/test/java/com/lucas/qa/tests/LoginComSucessoTest.java
@@ -4,42 +4,55 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import com.lucas.qa.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import com.lucas.qa.drivers.DriverFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 
 public class LoginComSucessoTest {
+
+    // Driver compartilhado por todos os métodos da classe
+    private WebDriver driver;
+
+    @BeforeEach
+    void setup(){
+
+        // Criar o Navegador
+        driver = DriverFactory.criarDriver();
+
+        // Acessar o Sistema
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterEach
+    void tearDown(){
+
+        // Fechar o navegador após cada teste
+        driver.quit();
+    }
 
     @Test
     void validarLoginComSucesso() throws InterruptedException {
 
-        // 1. Obtém uma instância do navegador através da fábrica
-        WebDriver driver = DriverFactory.criarDriver();
-
-        // 2. Abre o Site
-        driver.get("https://www.saucedemo.com/");
-
-        // 3. Criar o objeto da página (aqui conectamos o navegador a página)
+        //  Criar o objeto da página (aqui conectamos o navegador a página)
         LoginPage loginPage = new LoginPage(driver);
 
-        // 4. Executar a ação
-        loginPage.realizaLogin("standard_user", "secret_sauce");
+        //  Executar a ação
+        loginPage.realizaLogin(
+                "standard_user",
+                "secret_sauce"
+        );
 
-        // 5. Obtém a URL atual após login
+        //  Obtém a URL atual após login
         String urlAtual = driver.getCurrentUrl();
 
-        // 6. Exibe a URL no console para fins de estudo
-        System.out.println("URL atual: " + urlAtual);
-
-        // 7. Verificar  se o usuário foi redirecionado para a página de produtos
+        //  Verificar  se o usuário foi redirecionado para a página de produtos
         Assertions.assertTrue(
                 urlAtual.contains("inventory"),
                 "Falha no login. Usuário não foi redirecionado para a página de produtos."
         );
 
-        // 8. Pausa
+        // Pausa
         Thread.sleep(3000);
 
-        // 9. Encerrar navegador
-        driver.quit();
     }
 }
