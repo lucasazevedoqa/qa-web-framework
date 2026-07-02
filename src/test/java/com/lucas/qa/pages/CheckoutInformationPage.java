2fb96f10f2e10d2fe611a593ff8cd51ca8f7d1e9
Lucas Azevedo <lucasworkingqa@gmail.com>
2026-07-02 20:37:47 -0300

    fix: estabiliza preenchimento das informações de checkout no CI

--- src/test/java/com/lucas/qa/pages/CheckoutInformationPage.java
+++ src/test/java/com/lucas/qa/pages/CheckoutInformationPage.java
@@ -2,7 +2,9 @@ package com.lucas.qa.pages;
 
 import com.lucas.qa.config.Config;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -20,18 +22,44 @@ public class CheckoutInformationPage {
 
     public CheckoutInformationPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(Config.TIMEOUT_PADRAO));
+        this.wait = new WebDriverWait(
+                driver,
+                Duration.ofSeconds(Config.TIMEOUT_PADRAO)
+        );
     }
 
     public void preencherDadosEContinuar(String nome, String sobrenome, String cep) {
         wait.until(ExpectedConditions.urlContains("checkout-step-one.html"));
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(campoNome)).sendKeys(nome);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(campoSobrenome)).sendKeys(sobrenome);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(campoCep)).sendKeys(cep);
+        preencherCampo(campoNome, nome);
+        preencherCampo(campoSobrenome, sobrenome);
+        preencherCampo(campoCep, cep);
 
-        wait.until(ExpectedConditions.elementToBeClickable(botaoContinue)).click();
+        clicarContinuar();
 
         wait.until(ExpectedConditions.urlContains("checkout-step-two.html"));
+    }
+
+    private void preencherCampo(By localizador, String valor) {
+        WebElement campo = wait.until(ExpectedConditions.visibilityOfElementLocated(localizador));
+
+        campo.clear();
+        campo.sendKeys(valor);
+    }
+
+    private void clicarContinuar() {
+        WebElement continuar = wait.until(ExpectedConditions.presenceOfElementLocated(botaoContinue));
+
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView({block: 'center'});",
+                continuar
+        );
+
+        wait.until(ExpectedConditions.elementToBeClickable(botaoContinue));
+
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].click();",
+                continuar
+        );
     }
 }
